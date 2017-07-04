f96dafb5fbd60c0a40df8c9e179f041d5ceeb276
Blanka Kovács <kovacsblanka06@gmail.com>
2017-07-04 13:25:14 +0200

    Refactoring

--- JS_Section8-12/JS_whileProblemSet.js
+++ JS_Section8-12/JS_whileProblemSet.js
@@ -1,37 +1,37 @@
-// print all number between -10 and 19
+console.log("print all number between -10 and 19");
 
-var num_1 = -10;
-while (num_1 <= 19) {
-    console.log(num_1);
-    num_1++;
+var counter = -10;
+while (counter <= 19) {
+    console.log(counter);
+    counter++;
 }
 
-// print all even numbers between 10 and 40
+console.log("print all even numbers between 10 and 40");
 
-var num_2 = 10;
-while (num_2 <= 40) {
-    if (num_2 % 2 === 0) {
-        console.log(num_2);
+var counter = 10;
+while (counter <= 40) {
+    if (counter % 2 === 0) {
+        console.log(counter);
     }
-    num_2++;
+    counter++;
 }
 
-// print all odd numbers between 300 and 333
+console.log("print all odd numbers between 300 and 333");
 
-var num_3 = 300;
-while (num_3 <= 333) {
-    if (num_3 % 2 !== 0) {
-        console.log(num_3);
+var counter = 300;
+while (counter <= 333) {
+    if (counter % 2 !== 0) {
+        console.log(counter);
     }
-    num_3++;
+    counter++;
 }
 
-// print all number divisible by 5 AND 3 between 5 and 50
+console.log("print all number divisible by 5 AND 3 between 5 and 50");
 
-var num_4 = 5;
-while (num_4 <= 50) {
-    if (num_4 % 5 === 0 && num_4 % 3 === 0) {
-        console.log(num_4);
+var counter = 5;
+while (counter <= 50) {
+    if (counter % 5 === 0 && counter % 3 === 0) {
+        console.log(counter);
     }
-    num_4++;
+    counter++;
 }
