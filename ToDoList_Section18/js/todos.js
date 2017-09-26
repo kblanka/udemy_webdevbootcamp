dac88c8b8770c86551b2b2d1e8cb27cee815d18b
Blanka Kovács <kovacsblanka06@gmail.com>
2017-09-26 16:37:20 +0200

    Create todos.js

---
+++ ToDoList_Section18/js/todos.js
@@ -0,0 +1,21 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+//click on X to delete ToDo element
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event){ 
+  if(event.which === 13){
+    //grabbing new todo text from input
+    var todoText= $(this).val();
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+  }
+});
