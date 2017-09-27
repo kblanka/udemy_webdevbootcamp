e15fc9e27a80caf6dee672f4087e445cd48d8b45
Blanka Kovács <kovacsblanka06@gmail.com>
2017-09-27 07:48:38 +0200

    added fadeToggle

--- ToDoList_Section18/assets/js/todos.js
+++ ToDoList_Section18/assets/js/todos.js
@@ -11,11 +11,18 @@ $("ul").on("click", "span", function(event){
   event.stopPropagation();
 });
 
-$("input[type='text'").keypress(function(event){ 
+//adding new elements to the list
+$("input[type='text']").keypress(function(event){ 
   if(event.which === 13){
     //grabbing new todo text from input
-    var todoText= $(this).val();
+    var todoText = $(this).val();
     //create a new li and add to ul
-    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    $("ul").append("<li><span><i class='fa fa-trash-o'></i></span> " + todoText + "</li>");
+    $(this).val("");
   }
+});
+
+//click on pencil button fade in and out the input field
+$(".fa-pencil").click(function(){
+  $("input[type='text']").fadeToggle();
 });
